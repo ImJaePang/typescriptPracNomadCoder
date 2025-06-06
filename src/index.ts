065d3f6f0ec550684b21689b443effeac7ef37d2
ImJaePang <narushel@naver.com>
2025-06-06 20:14:39 +0900

    genericForClasses

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,16 @@
-type SuperPrint1 = <T,M>(a: T[], b?: M) => T;
-
-const superPrint1 : SuperPrint1 = (a) => a[];
-
-const a = superPrint1([1,2,3,4]);
-const b = superPrint1(["aa", "bb", "cc"]);
-const c = superPrint1([false, true, false]);
-const d = superPrint1([1, 2, "string", false]);
-
-
-function superPrint2<T>(a: T[]){
-    return a[0];
+type Player<E> = {
+    name :string,
+    extraInfo : E
 }
 
+const nico : Player<{favFood : string}> = {
+    name : "nico",
+    extraInfo : {
+        favFood : "kimchi"
+    }
+}
 
-const aa = superPrint2([1,2,3,4]);
-const bb = superPrint2(["aa", "bb", "cc"]);
-const cc = superPrint2([false, true, false]);
-const dd = superPrint2([1, 2, "string", false]);
-
+const lynn : Player<null> = {
+    name : "lynn",
+    extraInfo : null
+}
