1b19f12ed3602b9645fb7e3ea43e7664897694ad
ImJaePang <narushel@naver.com>
2025-06-06 19:47:34 +0900

    genericCon1

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
-type SuperPrint = <T,M>(a: T[], b: M) => T;
+type SuperPrint1 = <T,M>(a: T[], b?: M) => T;
 
-const superPrint : SuperPrint = (a) => a[];
+const superPrint1 : SuperPrint1 = (a) => a[];
 
-const a = superPrint([1,2,3,4], "bb");
-const b = superPrint(["aa", "bb", "cc"], 3);
-const c = superPrint([false, true, false], true);
-const d = superPrint([1, 2, "string", false], "aaaa");
+const a = superPrint1([1,2,3,4]);
+const b = superPrint1(["aa", "bb", "cc"]);
+const c = superPrint1([false, true, false]);
+const d = superPrint1([1, 2, "string", false]);
+
+
+function superPrint2<T>(a: T[]){
+    return a[0];
+}
+
+
+const aa = superPrint2([1,2,3,4]);
+const bb = superPrint2(["aa", "bb", "cc"]);
+const cc = superPrint2([false, true, false]);
+const dd = superPrint2([1, 2, "string", false]);
 
