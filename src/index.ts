fd03d4096c4245324a391d517ec559f6feb84f0d
ImJaePang <narushel@naver.com>
2025-06-06 13:13:48 +0900

    learnGeneric

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
-const greet = (name:string) : string =>{
-    return `hello, ${name}`;
-}
+type SuperPrint = <T,M>(a: T[], b: M) => T;
 
-console.log(greet("jaeyoung!"));
+const superPrint : SuperPrint = (a) => a[];
+
+const a = superPrint([1,2,3,4], "bb");
+const b = superPrint(["aa", "bb", "cc"], 3);
+const c = superPrint([false, true, false], true);
+const d = superPrint([1, 2, "string", false], "aaaa");
+
