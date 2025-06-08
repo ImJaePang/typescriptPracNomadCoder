d09a14f5067c5bc4d92f614ea42fcfc921eb0b1d
ImJaePang <narushel@naver.com>
2025-06-08 16:21:30 +0900

    interFaceOne

--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,13 @@
-type Words = {
-    [key:string] : string,
+type Team = "red" | "blue" | "yellow"
 
-}
 
-// let dict : Words = {
-//     "potato" : "food"
-// }
-
-class Dict {
-    private words :Words
-    constructor(){
-        this.words = {}
-    }
-
-    add(word:Word){
-        if (this.words[word.term] === undefined){
-            this.words[word.term] = word.def;
-        }   
-    }
-
-    def(term:string){
-        if (this.words[term] !== undefined){
-            return this.words[term];
-        } else {
-            return "no def this words"
-        }
-    }
-
-    update(word:Word){
-        if(this.words[word.def] !== undefined){
-            this.words[word.def] = word.term;
-        }
-    }
-
-    delete(term:string){
-        if(this.words[term] !== undefined){
-            // this.words[term] = undefined
-        }
-    }
+interface Player {
+    nickName : string,
+    team: Team
 }
 
 
-class Word {
-    constructor (
-        public term : string,
-        public def : string
-    ){}
+const nico :Player = {
+    nickName : "nico"
+    ,team : "red"
 }
-
-const kimchi = new Word("kimchi", "한국의 음식");
-
-const dict = new Dict();
-
-dict.add(kimchi);
-
-console.log(dict.def("kimchi"));
-console.log(dict.def("kimchi111"));
-
