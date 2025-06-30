a7bcf72ed5abd5aeb39105e3d98882d89cc18aba
ImJaePang <narushel@naver.com>
2025-06-30 21:57:53 +0900

    useDtsFileForDefinition

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
-const hello = () => "h!!";
+import {init, exit} from "myPackage";
 
-class Block {
-    constructor(private data: string) {}
-    static hello(){
-        return "hi";
-    }
-}
+init({
+    url:"true"
+})
+
+exit(1);
